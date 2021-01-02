c2722c4a092bbf961c736e85ff6acc1a2e7a0116
park yongmin <pym505@naver.com>
2021-01-03 00:24:10 +0900

    cote

--- programers_hash_2.py
+++ programers_hash_2.py
@@ -1,32 +1,18 @@
-//해시테이블문제1문이다 첫번째로 해시를사용하는데 객체를하나만들어 해당해시의 값을조절하여 답을출력한다
+def solution(p):
+    answer=True
+p.sort()
+for i in range(0,len(p)):
+    for a in p[i+1:]:
+        if p[i] in a:
+            answer = False
+            return answer
+return answer
 
-function solution(p, c) {
-    //var answer = '';
-    let r = [];
-    //객체생성
+phone_book = ["119", "97674223", "1195524421"]
+print(solution(phone_book))
 
-    for (let i in p) {
-        r[p[i]] = 0;
-    }
-    //참가자만큼의값초기화
+phone_book = ["12", "123", "1235", "567", "88"]
+print(solution(phone_book))
 
-    for (let i in p) {
-        r[p[i]]++;
-    }
-    //참가자만큼의값을++해준다. 왜냐면중복되는사람도있으므로 A가3번 중복참가인데 완주에서2번밖에안나오면
-    // 절대값으로하면에러
-
-    for (let i in c) {
-        r[c[i]]--;
-    }
-    //참가자값을-- 해준다 
-
-    for (let i in r) {
-        if (r[i] > 0) {
-            return i;
-        }
-    }
-
-    //값이0 이상이면리턴해준다
-    //return answer;
-}
+phone_book = ["123", "456", "789"]
+print(solution(phone_book))
