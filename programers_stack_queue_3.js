ce7dae864431c9fdc0dd85a858f1556be2be6b86
park yongmin <pym505@naver.com>
2021-01-10 22:40:04 +0900

    cote

--- programers_stack_queue_3.js
+++ programers_stack_queue_3.js
@@ -1,4 +1,4 @@
-"""
+/*
 문제 설명
 트럭 여러 대가 강을 가로지르는 일 차선 다리를 정해진 순으로 건너려 합니다. 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 알아내야 합니다. 트럭은 1초에 1만큼 움직이며, 다리 길이는 bridge_length이고 다리는 무게 weight까지 견딥니다.
 ※ 트럭이 다리에 완전히 오르지 않은 경우, 이 트럭의 무게는 고려하지 않습니다.
@@ -27,68 +27,21 @@ bridge_length	weight	truck_weights	return
 2	            10	    [7,4,5,6]	    8
 100         	100	    [10]    	    101
 100	            100	    [10,10,10,10,10,10,10,10,10,10]	110
-"""
-#일단 다리는 건너는중인 리스트를 만들고 대기중인리스트를만들자. 그리고 완료는뭐...있음좋고 
+#
+일단 다리는 건너는중인 리스트를 만들고 대기중인리스트를만들자.그리고 완료는뭐...있음좋고
 
-def func(bl, w, tw):
-    #bl은 다리길이 , w는 다리무게, tw 트럭무게
-    re= 0
-    ing_list=[]
-    comp_list=[]
-    time=0
+*/
 
-    for i in range(0,len(tw)):
-        while ingk_list th: 
-        target = tw.pop(0)
-        ing_list.append(target)
-        time=time+1
-            if len(bl) = target:
-
-
-            #target = tw.pop(0)
-            #print(tw.pop(0))
-            
-            #print()
-
-            #target = tw.pop(0)
-            #ing_list.append(target)
-            #time=time+1
-             
-            """
-            target = tw.pop(0)
-            ing_list.append(target)
-            time=time+1
-
-            target = tw.pop(0)
-            ing_list.append(target)
-            time=time+1
-            """
-        #다리에 넣는다 
-        
-        #print(comp_list,ing_list,tw,"-",time)
-        #print(comp_list,ing_list,tw,"-",time)
-
-
-    print(re)
-    return re
-
-bl=2
-w=10
-tw=[7,4,5,6]
-# 8 
-func(bl,w,tw)
+bl = 2
+w = 10
+tw = [7, 4, 5, 6] // 8
+func(bl, w, tw)
 exit
 
-bl=100
-w=100
-tw=[10]
-# 101
-#func(bl,w,tw)
+bl = 100
+w = 100
+tw = [10] //101# func(bl, w, tw)
 
-bl=100
-w=100
-tw=[10,10,10,10,10,10,10,10,10,10]
-# 110
-#func(bl,w,tw)
-
-
+bl = 100
+w = 100
+tw = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10] //# 110# func(bl, w, tw)
